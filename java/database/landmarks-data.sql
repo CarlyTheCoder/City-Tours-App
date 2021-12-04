8bea9a48150a90fb0f86ea0599ba475407d88d95
Kirk Baglia <kirkbaglia@gmail.com>
2021-12-04 10:07:33 -0500

    DB data update

--- java/database/landmarks-data.sql
+++ java/database/landmarks-data.sql
@@ -7,4 +7,4 @@ VALUES ('Comerica Park', 'stadium', '2100 Woodward Ave, Detroit, MI 48201', '(42
         ('Campus Martius', 'park', '800 Woodward Ave, Detroit, MI 48226', '(42.33246786894614, -83.0465565)', 6, 22),
         ('"The Fist"', 'monument', '5 Woodward Ave, Detroit, MI 48226', '(42.32943650844858, -83.04466454602776)', 0, 24),
         ('Detroit Institute of Arts', 'museum', '5200 Woodward Ave, Detroit, MI 48202', '(42.36015342142897, -83.06451619666238)', 10, 16),
-        ('Belle Isle', 'museum', 'Belle Isle, Detroit, Michigan', '(42.34569298114107, -82.97408487582452)', 5, 22);
+        ('Belle Isle', 'park', 'Belle Isle, Detroit, Michigan', '(42.34569298114107, -82.97408487582452)', 5, 22);
