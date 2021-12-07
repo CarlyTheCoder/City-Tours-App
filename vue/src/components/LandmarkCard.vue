14488a6d83aacb90788419e9882a054825ab28ff
Garrett John Law <law.garrettjohn@gmail.com>
2021-12-07 13:00:53 -0500

    updated landmark card for description

--- vue/src/components/LandmarkCard.vue
+++ vue/src/components/LandmarkCard.vue
@@ -1,14 +1,19 @@
 <template>
 <div id="landmark-card">
-  <div id="left">
-    <h1 id="title">  {{landmark.name}}</h1>
-    <p id="category"> <b>Category: </b>{{landmark.category}} </p>
+  <div>
+  <img id="image" v-bind:src="landmark.image" alt="">
   </div>
-  
-  <div id="right">
+ 
+  <div id="left">
+    <h2 id="title">  {{landmark.name}}</h2>
+    <p id="category"> <b>Category: </b>{{landmark.category}} </p>
     <p id="address"> <b>Address: </b>  {{landmark.address}} </p>
     <p id="hours"><b>Hours: </b>  {{landmark.openFrom}} through {{landmark.openTo}} </p>
+    <p>Lorem ipsum dolor sit amet, fugit ignota phaedrum ut duo. Sit volumus suavitate ut, et labitur appareat repudiare vix. An sed denique appetere probatus. Dolor ocurreret pertinacia vel ex, error prodesset mea cu. Ex sea amet probatus gloriatur, ad sit movet ceteros assentior. Ne illum quaeque vis, munere singulis sit eu, quo no graecis indoctum.
+ {{landmark.description}} </p>
   </div>
+  
+ 
  
   </div>
 </template>
@@ -34,18 +39,34 @@ props: ['landmark'],
   border-radius: 5px;
   width: 75%;
   display: grid;
-  grid-auto-columns: 1fr 1fr;
-  grid-template-areas: "left right";
+  grid-auto-columns: 275px 1fr;
+  grid-template-areas: "image left";
 }
 #left {
   grid-area: left;
+  
 }
+
 #right {
   grid-area: right;
   display: flex;
-  align-items: flex-end;
+  align-items: flex-start;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: flex-start;
+}
+#title {
+  padding: 0px;
+  margin: 0;
+  font-size: 1.8em;
+}
+#category, #address, #hours {
+  font-size: 1em;
+  margin: 0px;
+}
+
+#image {
+  grid-area: image;
+  width: 250px;
 }
 
 
