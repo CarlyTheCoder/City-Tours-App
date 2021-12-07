bbbcfbddc8ef8f601680c1513642e98a247b4ad2
Garrett John Law <law.garrettjohn@gmail.com>
2021-12-07 11:46:46 -0500

    added images to landmark cards

--- java/database/landmarks-data.sql
+++ java/database/landmarks-data.sql
@@ -1,10 +1,10 @@
 INSERT INTO landmarks (name, category, address, latitude, longitude, open_from, open_to, image)
-VALUES ('Comerica Park', 'stadium', '2100 Woodward Ave, Detroit, MI 48201', 42.3393471877925, -83.0494890306972, 0, 24, 'https://imgur.com/08AMcpW'),
-        ('Ford Field', 'stadium', '2000 Brush St, Detroit, MI 48226', 42.34056152112835, -83.04543133891744, 0, 24, 'https://imgur.com/R9dhWOy'),
-        ('Little Caesars Arena', 'stadium', '2645 Woodward Ave, Detroit, MI 48201', 42.341784593385626, -83.0552673, 0, 24, 'https://imgur.com/RgB3M7c'),
-        ('TCF Center', 'convention center', '1 Washington Blvd, Detroit, MI 48226', 42.32720636711678, -83.04946451534258, 0, 24, 'https://imgur.com/nw6sunk'),
-        ('GM Renaissance Center', 'business', 'Renaissance Center, 400 Renaissance Dr W, Detroit, MI 48243', 42.32955507212509, -83.04018591534258, 0, 24, 'https://imgur.com/mgDHJTO'),
-        ('Campus Martius', 'park', '800 Woodward Ave, Detroit, MI 48226', 42.33246786894614, -83.0465565, 6, 22, 'https://imgur.com/vukEizH'),
-        ('"The Fist"', 'monument', '5 Woodward Ave, Detroit, MI 48226', 42.32943650844858, -83.04466454602776, 0, 24, 'https://imgur.com/94yzCFw'),
-        ('Detroit Institute of Arts', 'museum', '5200 Woodward Ave, Detroit, MI 48202', 42.36015342142897, -83.06451619666238, 10, 16, 'https://imgur.com/Tro2fLi'),
-        ('Belle Isle', 'park', 'Belle Isle, Detroit, Michigan', 42.34569298114107, -82.97408487582452, 5, 22, 'https://imgur.com/GhJjaV6');
+VALUES ('Comerica Park', 'stadium', '2100 Woodward Ave, Detroit, MI 48201', 42.3393471877925, -83.0494890306972, 0, 24, 'https://i.imgur.com/08AMcpW.jpg?1'),
+        ('Ford Field', 'stadium', '2000 Brush St, Detroit, MI 48226', 42.34056152112835, -83.04543133891744, 0, 24, 'https://i.imgur.com/R9dhWOy.jpg?1'),
+        ('Little Caesars Arena', 'stadium', '2645 Woodward Ave, Detroit, MI 48201', 42.341784593385626, -83.0552673, 0, 24, 'https://i.imgur.com/RgB3M7c.jpg?1'),
+        ('TCF Center', 'convention center', '1 Washington Blvd, Detroit, MI 48226', 42.32720636711678, -83.04946451534258, 0, 24, 'https://i.imgur.com/nw6sunk.jpg?1'),
+        ('GM Renaissance Center', 'business', 'Renaissance Center, 400 Renaissance Dr W, Detroit, MI 48243', 42.32955507212509, -83.04018591534258, 0, 24, 'https://i.imgur.com/mgDHJTO.jpg?1'),
+        ('Campus Martius', 'park', '800 Woodward Ave, Detroit, MI 48226', 42.33246786894614, -83.0465565, 6, 22, 'https://i.imgur.com/vukEizH.jpg'),
+        ('"The Fist"', 'monument', '5 Woodward Ave, Detroit, MI 48226', 42.32943650844858, -83.04466454602776, 0, 24, 'https://i.imgur.com/94yzCFw.jpg?1'),
+        ('Detroit Institute of Arts', 'museum', '5200 Woodward Ave, Detroit, MI 48202', 42.36015342142897, -83.06451619666238, 10, 16, 'https://i.imgur.com/Tro2fLi.jpg?1'),
+        ('Belle Isle', 'park', 'Belle Isle, Detroit, Michigan', 42.34569298114107, -82.97408487582452, 5, 22, 'https://i.imgur.com/GhJjaV6.png?1');
