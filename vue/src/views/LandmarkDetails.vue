962d5ce709a6c998239ce35a5c53ab8ee41c80ca
Kirk Baglia <kirkbaglia@gmail.com>
2021-12-09 13:29:56 -0500

    frontend card fix

--- vue/src/views/LandmarkDetails.vue
+++ vue/src/views/LandmarkDetails.vue
@@ -1,27 +1,36 @@
 <template>
     <div>
-        <landmark-card v-for="landmark in thisLandmark" v-bind:key="landmark.id" :landmark="landmark"></landmark-card>
+        <landmark-card v-bind:landmark="this.landmark"></landmark-card>
     </div>
 </template>
 
 <script>
+import landmarkCard from '@/components/LandmarkCard'
 import landmarkService from '@/services/LandmarkService';
 export default {
+    components: {
+        landmarkCard
+    },
+    data() {
+        return {
+            landmark: {}
+        }
+    },
     created() {
         const thisId = this.$route.params.id;
         // this.$store.commit("SET_ACTIVE_LANDMARK_ID", thisId);
         this.getLandmark(thisId);
     },
-    computed: {
-        thisLandmark() {
-            return this.$store.state.landmarks;
-            // .find(landmark => landmark.id == this.$store.state.activeLandmarkId)
-        }
-    },
+    // computed: {
+    //     thisLandmark() {
+    //         return this.$store.state.landmarks;
+    //         // .find(landmark => landmark.id == this.$store.state.activeLandmarkId)
+    //     }
+    // },
     methods: {
         getLandmark(id) {
             landmarkService.getById(id).then((response) => {
-                this.$store.commit("POPULATE_LANDMARKS", response.data)
+                this.landmark = response.data;
             })
         }
     }
