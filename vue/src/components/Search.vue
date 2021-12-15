7cf6947f5e34eb65b7a24efdd94226757fd8a767
Carly Chabal <carlychabal@gmail.com>
2021-12-15 09:51:06 -0500

    visual changes

--- vue/src/components/Search.vue
+++ vue/src/components/Search.vue
@@ -10,7 +10,7 @@
             <select name="category" id="search-category" class="input-field" v-model="filter.category" v-on:change="filterLandmarks()">
                 <option value=""></option>
                 <option value="business">Business</option>
-                <option value="convention-center">Convention Center</option>
+                <option value="convention center">Convention Center</option>
                 <option value="monument">Monument</option>
                 <option value="museum">Museum</option>
                 <option value="park">Park</option>
@@ -74,11 +74,12 @@ export default {
 <style>
 
 .search-element {
-    padding: 5px;
+    padding: 10px;
     display: flex;
     flex-direction: row;
     align-items: center;
     column-gap: 15px;
+    font-weight: bold;
 }
 
 #search-name, #search-category, #search-day {
