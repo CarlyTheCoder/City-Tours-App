4b2b5f1b7d9402cfd4ff9ae928b03b8197491177
Garrett John Law <law.garrettjohn@gmail.com>
2021-12-16 12:20:38 -0500

    conditional formatting for itinerary preview cards

--- vue/src/components/ItineraryPreview.vue
+++ vue/src/components/ItineraryPreview.vue
@@ -2,9 +2,22 @@
 
   <router-link v-bind:to="{ name: 'itinerary', params: {id: itinerary.id} }">
     <div id="itinerary-preview">
-      <h2> {{itinerary.name}}</h2>
-      <p>Date: {{itinerary.tripDate}}</p>
-      <p>Number of stops: {{landmarkCount}}</p>
+      <div id="itin-preview-left">
+        <h2> {{itinerary.name}}</h2>
+        <p>Date: {{itinerary.tripDate}}</p>
+        <p>Number of stops: {{landmarkCount}}</p>
+      </div>
+      <div id="itin-preview-right">
+        <div v-if="landmarkCount > 0">
+          <img v-bind:src="firstlandmark.image" alt="" id="first-landmark-img">
+        </div>
+        <div v-else id="no-landmarks">
+          <p>You don't have any landmarks on your itinerary yet!</p>
+          <button class="button">Search landmarks</button>
+        </div>
+      </div>
+      
+     
     </div>
   </router-link>
 
@@ -17,6 +30,9 @@ name: "itinerary-preview",
   computed: {
     landmarkCount() {
       return this.itinerary.landmarks.length;
+    },
+    firstlandmark() {
+      return this.itinerary.landmarks[0];
     }
   }
 }
@@ -33,9 +49,11 @@ name: "itinerary-preview",
   justify-content: flex-start;
   row-gap: 15px;
   max-width: 70%;
-  min-width: 300px;
+  min-width: 650px;
   margin: auto;
   margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
 }
 
 #itinerary-preview:hover {
@@ -48,6 +66,25 @@ name: "itinerary-preview",
 #itinerary-preview h2 {
   font-size: 2rem;
   margin: 5px 0;
+}
+
+#itin-preview-right {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
+#first-landmark-img {
+  height: 150px;
+  box-shadow: 1px 1px 4px #adadad;
+}
+
+#no-landmarks {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 </style>
